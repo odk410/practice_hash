cb91fe281304ca32bba07160e0ff16a4e3ab8969
odk410 <odk410@naver.com>
2018-01-18 10:01:16 +0900

    practice hash

---
+++ practice_hash.rb
@@ -0,0 +1,14 @@
+student = {
+  name: "john",
+  age: 19,
+  gender: "male"
+  # school: {
+  #   highschool: ["PFLHS", "German"],
+  #   college: ["yonsei", "CS"],
+  #   graduate: ["KAIST", "CS"]
+  # }
+}
+
+puts student[:age]
+puts student[:gender]
+# puts student[:school]
